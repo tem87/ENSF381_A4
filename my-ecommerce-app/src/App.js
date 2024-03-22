b041a148bbfe7d71400795787fb3a715f0582c68
tem87 <thaesp01@gmail.com>
2024-03-21 23:07:43 -0600

    chnages

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './component/Homepage'; // Assuming Homepage component file location
-import ProductPage from './component/ProductsPage'; // Import other page components if needed
+//import ProductPage from './component/ProductPage'; // Import other page components if needed
 //import LoginPage from './component/LoginPage';
 
 
@@ -15,8 +15,6 @@ function App() {
       </Routes>
     </BrowserRouter>
     
-
-  
 
   );
 };
